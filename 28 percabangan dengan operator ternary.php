8787c9b400093f6e288b3fa5f98e93d9d531a1f5
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 21:24:28 -0700

    28 percabangan dengan operator ternary.php

---
+++ 28 percabangan dengan operator ternary.php
@@ -0,0 +1,11 @@
+<?php
+
+$suka = true;
+echo $suka ? "dia tidak suka kamu": "Baiklah!";
+echo "<hr>";
+
+$suka = false;
+echo $suka ? "Dia tidak suka kamu": "Baiklah!";
+echo "<hr>";
+echo "<p><strong>By:arif alfazhar";
+?>
