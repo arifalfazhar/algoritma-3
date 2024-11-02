d9d82cb624101f08f87ac72f8f27f647978fa84a
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 20:31:30 -0700

    01 variabel.php

---
+++ 01 variabel.php
@@ -0,0 +1,19 @@
+<?php
+
+$nama = 'arif';
+echo "Halo, $nama!<br><br>"; // Hasil: Halo, arif!
+
+//Menampilkan Variabel
+$umur = 17;
+echo "Umur saya adalah $umur tahun.<br><br>";
+
+//Menggabungkan Teks dan Variabel
+$depan = 'arif';
+$belakang = 'alfazhar';
+echo "Nama saya adalah " . $depan . " " . $belakang . ".<br><br>";
+
+//Menggunakan Echo untuk HTML
+echo "<h1>Judul Halaman</h1>";
+echo "<p>Paragraf ini dibuat dengan PHP.</p>";
+
+echo "<strong>By:arif alfazhar";
