af15ce5528745a4f5c276a627ee3e9997fe11c69
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 20:53:18 -0700

    12 var_dump dari luar negeri.php

---
+++ 12 var_dump dari luar negeri.php
@@ -0,0 +1,12 @@
+<?php
+
+$array = array(
+    'name' => 'julian',
+    'age' => 23,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+
+var_dump($array);
+ 
+$array = '<p><strong>By:arif alfazhar';
+var_dump($array);
