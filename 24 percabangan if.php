23fbc59416f9476a78bc62711f58930958aa2902
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 21:18:11 -0700

    24 percabangan if.php

---
+++ 24 percabangan if.php
@@ -0,0 +1,26 @@
+
+
+<?php
+$total_belanja = 150000;
+
+if($total_belanja > 100000){
+    echo "Anda dapat hadiah sebesar 75%!";
+    echo "<hr>";
+}
+
+$totalbelanja =150000;
+
+if($totalbelanja > 100000){
+    echo "Anda dapat belanja dengan nyaman menggunakan voucher
+    sebesar 200000!";
+    echo "<hr>";
+}
+
+$totalBelanja =230000;
+
+if($totalBelanja > 100000){
+    echo "Jika anda belanja buah di hari selasa maka akan 
+    dapat potongan harga RP 30000!";
+    echo "<hr>";
+}
+echo "<p><strong>By:Dara arif alfazhar";
