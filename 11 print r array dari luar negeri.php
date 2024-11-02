8f77a2a18ca4b1d139a016b2fb5d665d7fbfc9a7
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 20:49:34 -0700

    11 print r array dari luar negeri.php

---
+++ 11 print r array dari luar negeri.php
@@ -0,0 +1,11 @@
+<?php
+
+$array = array(
+    'name' => 'julian',
+    'age' => 23,
+    'skills' => array('PHP', 'JavaScript', 'HTML')
+);
+print_r($array);
+
+$array = '<p><strong>By:arif alfazhar';
+print_r($array);
