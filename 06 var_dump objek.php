9bbc0846b6fdfabd6b758ad436f9ae542bd4279d
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 20:41:31 -0700

    06 var_dump objek.php

---
+++ 06 var_dump objek.php
@@ -0,0 +1,13 @@
+<?php
+class motor {
+    public $jenis;
+    public $warna;
+}
+
+$nama_bunga = new Bunga();
+$nama_bunga->jenis = "rx king ";
+$nama_bunga->warna = "black";
+
+var_dump($nama_motor);
+echo "<br><br><strong>By:arif alfazhar";
+?>
