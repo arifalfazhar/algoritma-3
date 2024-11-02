35d11c8a9b9c4974874122412e077e558181162f
arifalfazhar <rokokksp03@gmail.com>
2024-11-01 20:33:32 -0700

    02 print_array.php

---
+++ 02 print_r array.php
@@ -0,0 +1,12 @@
+<?php
+
+//Contoh Penggunaan dengan Variabel Biasa
+$nama = "arif<p>";
+print_r($nama);
+
+//Hasil:
+//arif
+//Contoh Penggunaan dengan Array
+
+$minuman =array ("susu","teh", "matcha", "Air air putih",);
+print_r($minuman);
